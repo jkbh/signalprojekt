b631b829c507f3aa79c7f8392c669228e5c16ea1
jkbh <jakob_h@outlook.de>
2023-08-19 10:29:27 +0200

    Change gcc function to single gccphat call

--- project_data/getGCCFeatures.m
+++ project_data/getGCCFeatures.m
@@ -1,8 +1,6 @@
 function features = getGCCFeatures(signals)
-    dFront = gccphat(signals(:,1), signals(:,3));
-    dBack = gccphat(signals(:,2), signals(:,4)); 
-    dLeft = gccphat(signals(:,1), signals(:,2));
-    dRight = gccphat(signals(:,3), signals(:,4));
-
-    features = [dFront dBack dLeft dRight];    
+    sig = [signals(:,1) signals(:,2) signals(:,1) signals(:,3)];
+    refsig = [signals(:,3) signals(:,4) signals(:,2) signals(:,4)];    
+    
+    features = gccphat(sig, refsig, 48000);    
 end
