2b8632a854ffd3a941ea834aee5e03f179835c3f
Jakob Horbank <jakob_h@outlook.de>
2023-08-21 11:29:40 +0200

    Features for 1024 frames and use hist counts for classification

--- project_data/classifier.m
+++ project_data/classifier.m
@@ -2,17 +2,30 @@
 [samples, labels] = generateData('./recordings', 10);
 
 %% Feature Extraction
-featureNames = ["Front" "Back" "Left" "Right"];
-[features] = cell2mat(cellfun(@getGCCFeatures, samples, 'UniformOutput', false));
+nSamples = length(samples);
+featuresFull = [];
+labelsFull = [];
+for iSample = 1:nSamples
+    featuresLocal = getGCCFeatures(samples{iSample});
+    featuresFull = [featuresFull; featuresLocal];
+    nBlocks = length(featuresLocal);
+    labelsFull = [labelsFull; repmat(labels(iSample), nBlocks, 1)];
+end
+%[features] = cell2mat(cellfun(@getGCCFeatures, samples, 'UniformOutput', false));
 
 %% Training
-tblTrain = array2table(features, 'VariableNames', featureNames);
-
-model = fitctree(tblTrain, labels);
+model = fitctree(featuresFull, labelsFull);
 validationLoss = cvloss(model);
 
 %% Testing
 [testSamples , testLabels] = loadTestSignals();
-testFeatures = cell2mat(cellfun(@getGCCFeatures, testSamples, 'UniformOutput', false));
-
-testLoss = loss(model, testFeatures, testLabels);
+nTestSamples = length(testSamples);
+testResults = NaN(nTestSamples, 2);
+for iSample = 1:nTestSamples
+    testFeatures = getGCCFeatures(testSamples{iSample});
+    testFeatures(~any(testFeatures, 2), :) = [];% Remove all zero rows (no voice)
+    testFeatures(any(abs(testFeatures) == 1024, 2), :) = []; % Remove all 1024 delaya (error)
+    testPreds = predict(model, testFeatures);
+    [m, pred] = max(histcounts(testPreds, 6));
+    testResults(iSample, :) = [pred testLabels(iSample)];
+end
--- project_data/getGCCFeatures.m
+++ project_data/getGCCFeatures.m
@@ -1,6 +1,16 @@
 function features = getGCCFeatures(signals)
-    sig = [signals(:,1) signals(:,2) signals(:,1) signals(:,3)];
-    refsig = [signals(:,3) signals(:,4) signals(:,2) signals(:,4)];    
-    
-    features = gccphat(sig, refsig, 48000);    
+    nSamples = length(signals);
+    blockSize = 1024;
+    nBlocks = floor(nSamples/blockSize);
+    signals(nBlocks * blockSize) = 0;
+    features = NaN(nBlocks-1, 4);
+    for iBlock = 1:nBlocks-1
+        index = iBlock * blockSize:(iBlock+1)*blockSize;
+        sig = [signals(index,1) signals(index,2) signals(index,1) signals(index,3)];
+        refsig = [signals(index,3) signals(index,4) signals(index,2) signals(index,4)];
+        features(iBlock, :) = gccphat(sig, refsig, 1);  
+    end
+
+    features(~any(features, 2), :) = [];% Remove all zero rows (no voice)
+    features(any(abs(features) == 1024, 2), :) = []; % Remove all 1024 delaya (error)
 end
